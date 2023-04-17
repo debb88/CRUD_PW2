d874b788556192b7a05108941408e764fee5a970
debb88 <meilianadebora@gmail.com>
2023-04-17 23:21:17 +0700

    perbaikan tampilan create.php

--- create.php
+++ create.php
@@ -1,37 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/create.css">
+    <link rel="icon" href="img/logowebc-modified.png" />
+    <script src="validation.js"></script>
+    <script
+      src="https://kit.fontawesome.com/4cd8de584e.js"
+      crossorigin="anonymous"
+    ></script>
+    <title>WebComic</title>
+  </head>
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-  <link rel="stylesheet" href="style.css" />
-  <link rel="stylesheet" href="login.css">
-  <link rel="icon" href="img/logowebc-modified.png" />
-  <script src="https://kit.fontawesome.com/4cd8de584e.js" crossorigin="anonymous"></script>
-  <title>WebComic</title>
-</head>
+  <body>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"
+    ></script>
 
-<!-- margin -->
+    <!-- header -->
 
-<style>
-
-  footer {
-    margin-top: 720px;
-  }
-  
-</style>
-
-<body>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
-
-  <!-- header -->
-
-  <?php include './components/navbar.php'; ?>
+    <?php include './components/navbar.php'; ?>
 
   <!-- logic -->
 
@@ -145,12 +144,11 @@
     <p class="create">Have account? Sign in <a href="login.php">here</a>.</p>
   </div>
 
+    <!-- footer -->
 
-  <!-- footer -->
-
-  <footer>
+    <footer>
     <?php include './components/footer.php'; ?>
-  </footer>
+    </footer>
 
     <?php
     if ($queryRegister->execute()) {
@@ -165,12 +163,11 @@
           document.location.href = './index.php';
         })
       </script>;
+
       <?php
       $conn->close();
     }
-
     ?>
 
-</body>
-
+  </body>
 </html>
